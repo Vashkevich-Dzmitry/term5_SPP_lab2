c5b61a77d10919c91ff8dbcc244f0c4817c960c6
dzmitry <dzmitry.vashkevich.a@gmail.com>
2022-11-29 00:56:29 +0300

    add float generator

---
+++ Faker/FloatGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker
+{
+    internal class FloatGenerator : IGenerator
+    {
+        public object Generate()
+        {
+            return (float)(new Random().NextDouble());
+        }
+
+        public Type GetGeneratorType()
+        {
+            return typeof(float);
+        }
+    }
+}
