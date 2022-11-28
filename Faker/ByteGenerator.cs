5085a4af7f58c0ffdeb61f5e401f8d564eb2c28d
dzmitry <dzmitry.vashkevich.a@gmail.com>
2022-11-29 00:54:26 +0300

    add byte generator

---
+++ Faker/ByteGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker
+{
+    internal class ByteGenerator : IGenerator
+    {
+        public object Generate()
+        {
+            return new Random().NextInt64(byte.MaxValue);
+        }
+
+        public Type GetGeneratorType()
+        {
+            return typeof(byte);
+        }
+    }
+}
