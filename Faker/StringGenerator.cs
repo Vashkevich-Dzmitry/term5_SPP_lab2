871cd3e152bf1abb14c7b8fe9dd41425015fd7d6
dzmitry <dzmitry.vashkevich.a@gmail.com>
2022-11-29 01:13:28 +0300

    add string generator

---
+++ Faker/StringGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker
+{
+    internal class StringGenerator : IGenerator
+    {
+        private const string validCharacters= "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕёЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        private const int maxStringLength = 16;
+        private const int minStringLength = 4;
+
+        public object Generate()
+        {
+            int stringLength = (int)new Random().NextInt64(minStringLength, maxStringLength);
+            char[] str = new char[stringLength];
+
+            for (int i = 0; i < stringLength; i++)
+            {
+                str[i] = validCharacters[new Random().Next(validCharacters.Length)];
+            }
+            return new string(str);
+        }
+
+        public Type GetGeneratorType()
+        {
+            return typeof(string);
+        }
+    }
+}
