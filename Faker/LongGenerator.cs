d4220b49eb9c94ead7cfe78bb49ee66bf5427a88
dzmitry <dzmitry.vashkevich.a@gmail.com>
2022-11-29 00:58:24 +0300

    add long generator

---
+++ Faker/LongGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker
+{
+    internal class LongGenerator : IGenerator
+    {
+        public object Generate()
+        {
+            return new Random().NextInt64();
+        }
+
+        public Type GetGeneratorType()
+        {
+            return typeof(long);
+        }
+    }
+}
